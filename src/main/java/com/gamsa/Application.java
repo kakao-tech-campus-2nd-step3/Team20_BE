3566d4092cab91abdccb5c28c18a47cee3d9aa43
5win <94297900+5win@users.noreply.github.com>
2024-09-20 23:22:08 +0900

    weekly: 3주차 작업 내용 머지 (#8)
    * [#2] init: 프로젝트 생성 및 의존성 추가 (#3)
    
    - 프로젝트 생성
    - 의존성 추가
    - H2 연결 및 테스트
    
    * [#6] feat: 봉사활동 조회 기능 추가 (#7)
    
    - 아직 모두 구현하지 못했음

--- src/main/java/com/gamsa/Application.java
+++ src/main/java/com/gamsa/Application.java
@@ -2,13 +2,9 @@ package com.gamsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-=======
-
->>>>>>> ea22f29 (init: 프로젝트 생성)
 @SpringBootApplication
 public class Application {
 
