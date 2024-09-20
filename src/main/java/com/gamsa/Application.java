1f88b1b0f5df4c603c425c9e5317bfbab2e594f6
5win <94297900+5win@users.noreply.github.com>
2024-09-20 23:18:13 +0900

    [#6] feat: 봉사활동 조회 기능 추가 (#7)
    - 아직 모두 구현하지 못했음

--- src/main/java/com/gamsa/Application.java
+++ src/main/java/com/gamsa/Application.java
@@ -2,7 +2,9 @@ package com.gamsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/gamsa/activity/dto/ActivityFindAllResponse.java
@@ -0,0 +1,10 @@
+package com.gamsa.activity.dto;
+
+import com.gamsa.activity.domain.Activity;
+
+public class ActivityFindAllResponse {
+
+    public static ActivityFindAllResponse from(Activity activity) {
+        return null;
+    }
+}
