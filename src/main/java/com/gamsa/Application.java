bab2798123b58bee4d00d9ba62f322a2ec03e073
5win <94297900+5win@users.noreply.github.com>
2024-09-23 18:34:17 +0900

    init: 프로젝트 생성
    [#2] init: 프로젝트 생성 및 의존성 추가 (#3) (#4)

--- src/main/java/com/gamsa/Application.java
+++ src/main/java/com/gamsa/Application.java
@@ -2,9 +2,13 @@ package com.gamsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+=======
+
+>>>>>>> ea22f29 (init: 프로젝트 생성)
 @SpringBootApplication
 public class Application {
 
