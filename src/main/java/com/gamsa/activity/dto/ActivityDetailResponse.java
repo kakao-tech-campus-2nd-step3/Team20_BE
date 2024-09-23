2437f29731e86089b468a04706171fa4ddf6abd7
5win <94297900+5win@users.noreply.github.com>
2024-09-23 11:38:04 +0900

    [#6] feat: 활동 리스트, 상세정보 응답 DTO 구현

--- src/main/java/com/gamsa/activity/dto/ActivityDetailResponse.java
+++ src/main/java/com/gamsa/activity/dto/ActivityDetailResponse.java
@@ -2,10 +2,49 @@ package com.gamsa.activity.dto;
 
 
 import com.gamsa.activity.domain.Activity;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class ActivityDetailResponse {
 
+    private Long actId;
+    private String actLocation;
+    private String description;
+    private LocalDateTime noticeStartDate;
+    private LocalDateTime noticeEndDate;
+    private LocalDateTime actStartDate;
+    private LocalDateTime actEndDate;
+    private int recruitTotalNum;
+    private boolean adultPossible;
+    private boolean teenPossible;
+    private boolean groupPossible;
+    private boolean onlinePossible;
+    private int actWeek;
+    private String actManager;
+    private String actPhone;
+    private String url;
+
     public static ActivityDetailResponse from(Activity activity) {
-        return null;
+        return ActivityDetailResponse.builder()
+            .actId(activity.getActId())
+            .actLocation(activity.getActLocation())
+            .description(activity.getDescription())
+            .noticeStartDate(activity.getNoticeStartDate())
+            .noticeEndDate(activity.getNoticeEndDate())
+            .actStartDate(activity.getActStartDate())
+            .actEndDate(activity.getActEndDate())
+            .recruitTotalNum(activity.getRecruitTotalNum())
+            .adultPossible(activity.isAdultPossible())
+            .teenPossible(activity.isTeenPossible())
+            .groupPossible(activity.isGroupPossible())
+            .onlinePossible(activity.isOnlinePossible())
+            .actWeek(activity.getActWeek())
+            .actManager(activity.getActManager())
+            .actPhone(activity.getActPhone())
+            .url(activity.getUrl())
+            .build();
     }
 }
--- src/main/java/com/gamsa/activity/dto/ActivityFindAllResponse.java
+++ src/main/java/com/gamsa/activity/dto/ActivityFindAllResponse.java
@@ -1,10 +1,31 @@
 package com.gamsa.activity.dto;
 
 import com.gamsa.activity.domain.Activity;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class ActivityFindAllResponse {
 
+    private Long actId;
+    private String actLocation;
+    private LocalDateTime noticeStartDate;
+    private LocalDateTime noticeEndDate;
+    private LocalDateTime actStartDate;
+    private LocalDateTime actEndDate;
+    private int recruitTotalNum;
+
     public static ActivityFindAllResponse from(Activity activity) {
-        return null;
+        return ActivityFindAllResponse.builder()
+            .actId(activity.getActId())
+            .actLocation(activity.getActLocation())
+            .noticeStartDate(activity.getNoticeStartDate())
+            .noticeEndDate(activity.getNoticeEndDate())
+            .actStartDate(activity.getActStartDate())
+            .actEndDate(activity.getActEndDate())
+            .recruitTotalNum(activity.getRecruitTotalNum())
+            .build();
     }
 }
