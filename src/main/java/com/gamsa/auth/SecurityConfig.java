8e2bb506042b49c9c453a8ab136205d4afac4be9
5win <94297900+5win@users.noreply.github.com>
2024-09-27 11:35:23 +0900

    [#22] feat: H2콘솔 위한 X-Frame-Options 비활성화

--- src/main/java/com/gamsa/auth/SecurityConfig.java
+++ src/main/java/com/gamsa/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +16,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .headers(headerConfig -> headerConfig.frameOptions(
+                HeadersConfigurer.FrameOptionsConfig::sameOrigin))
             .authorizeHttpRequests(authorizeRequest -> {
                 authorizeRequest
                     .anyRequest().permitAll();
