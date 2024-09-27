700295be5afb40887f1eff780cc78462326b856c
Awhn <ahnshwan@outlook.com>
2024-09-28 00:53:46 +0900

    test: 아바타 관련 단위 테스트

---
+++ src/test/java/com/gamsa/avatar/repository/AvatarJpaRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.gamsa.avatar.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gamsa.avatar.constant.AgeRange;
+import com.gamsa.avatar.constant.Experienced;
+import com.gamsa.avatar.entity.AvatarJpaEntity;
+import com.gamsa.common.config.TestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDateTime;
+
+@DataJpaTest
+@Import(TestConfig.class)
+public class AvatarJpaRepositoryTest {
+    @Autowired
+    private AvatarJpaRepository avatarJpaRepository;
+
+    private final AvatarJpaEntity jpaEntity = AvatarJpaEntity.builder()
+            .avatarId(1L)
+            .avatarLevel(1L)
+            .avatarExp(1L)
+            .nickname("닉네임")
+            .ageRange(AgeRange.ADULT)
+            .experienced(Experienced.NOVICE)
+            .updateDate(LocalDateTime.now())
+            .build();
+
+    @Test
+    void 새로운_유저_저장() {
+        //when
+        avatarJpaRepository.save(jpaEntity);
+
+        //then
+        assertThat(avatarJpaRepository.findById(1L).get().getNickname()).isEqualTo(jpaEntity.getNickname());
+    }
+
+    @Test
+    void 저장된_유저_삭제() {
+        //given
+        avatarJpaRepository.save(jpaEntity);
+
+        //when
+        avatarJpaRepository.deleteById(1L);
+
+        //then
+        assertThat(avatarJpaRepository.findById(1L)).isEmpty();
+    }
+}
---
+++ src/test/java/com/gamsa/avatar/stub/StubAvatarRepository.java
@@ -0,0 +1,32 @@
+package com.gamsa.avatar.stub;
+
+import com.gamsa.avatar.constant.AgeRange;
+import com.gamsa.avatar.constant.Experienced;
+import com.gamsa.avatar.domain.Avatar;
+import com.gamsa.avatar.repository.AvatarRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class StubAvatarRepository implements AvatarRepository {
+    private final Avatar avatar = Avatar.builder()
+            .avatarId(1L)
+            .avatarLevel(1L)
+            .avatarExp(1L)
+            .nickname("닉네임")
+            .ageRange(AgeRange.ADULT)
+            .experienced(Experienced.NOVICE)
+            .updateDate(LocalDateTime.now())
+            .build();
+
+    @Override
+    public void save(Avatar avatar) {}
+
+    @Override
+    public Optional<Avatar> findById(Long id) {
+        return Optional.of(avatar);
+    }
+
+    @Override
+    public void deleteById(Long id) {}
+}
