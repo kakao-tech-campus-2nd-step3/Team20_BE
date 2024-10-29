6f5e976a73f2e97927b0ed6b6bd3dd15949ffce9
Awhn <69659322+Awhn@users.noreply.github.com>
2024-10-29 13:32:48 +0900

    fix: 아바타 저장 시 오류 해결 (#44)
    * feat: 유저 기록 기능 구현
    
    * test: 유저 기록 기능 테스트
    
    * fix: activity 관련 의존성 수정
    
    * fix: 요구사항 변경에 따른 선택적 무한 스크롤
    
    * fix: 아바타 역직렬화 과정에서의 오류 수정

--- src/main/java/com/gamsa/avatar/constant/AgeRange.java
+++ src/main/java/com/gamsa/avatar/constant/AgeRange.java
@@ -1,5 +1,7 @@
 package com.gamsa.avatar.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +13,19 @@ public enum AgeRange {
     ADULT("성인");
 
     private final String name;
+
+    @JsonCreator
+    public static AgeRange fromValue(String value) {
+        for (AgeRange ageRange : AgeRange.values()) {
+            if (ageRange.name.equals(value)) {
+                return ageRange;
+            }
+        }
+        throw new IllegalArgumentException("Unknown value: " + value);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name;
+    }
 }
