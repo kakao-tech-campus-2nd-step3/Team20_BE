7b1d16a7d8d5833acf57e3bb91b3ac6507166d00
5win <94297900+5win@users.noreply.github.com>
2024-10-29 14:29:12 +0900

    [#19] fix: history URI 수정 및 불필요한 test 삭제
    - /api/v1/history -> /api/v1/histories
    - ApplicationTest.java 삭제

--- src/main/java/com/gamsa/history/controller/HistoryController.java
+++ src/main/java/com/gamsa/history/controller/HistoryController.java
@@ -10,11 +10,18 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/history")
+@RequestMapping("/api/v1/histories")
 public class HistoryController {
     private HistoryService historyService;
 
