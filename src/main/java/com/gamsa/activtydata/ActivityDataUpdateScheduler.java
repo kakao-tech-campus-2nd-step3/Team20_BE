7204f29006039dab73a446b289e642afb8eb9c27
Awhn <ahnshwan@outlook.com>
2024-11-10 19:18:18 +0900

    fix: 잘못된 파일 삭제

--- src/main/java/com/gamsa/activtydata/ActivityDataUpdateScheduler.java
+++
@@ -1,9 +0,0 @@
-package com.gamsa.activtydata;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableScheduling
-public class ActivityDataUpdateScheduler {
-}
