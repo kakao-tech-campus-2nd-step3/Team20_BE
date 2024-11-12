db717f4afb0dd3c084b06bffe9d0d957f55360d2
Awhn <ahnshwan@outlook.com>
2024-11-12 21:54:36 +0900

    fix: 테스트 코드 상 위경도가 반대로 작성된 부분 수정

--- src/test/java/com/gamsa/activity/repository/InstituteJpaRepositoryTest.java
+++ src/test/java/com/gamsa/activity/repository/InstituteJpaRepositoryTest.java
@@ -1,15 +1,16 @@
 package com.gamsa.activity.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.gamsa.activity.entity.InstituteJpaEntity;
 import com.gamsa.common.config.TestConfig;
-import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(TestConfig.class)
@@ -19,13 +20,13 @@ class InstituteJpaRepositoryTest {
     private InstituteJpaRepository instituteJpaRepository;
 
     private final InstituteJpaEntity jpaEntity = InstituteJpaEntity.builder()
-        .instituteId(1L)
-        .name("도서관")
-        .location("서울시")
-        .latitude(new BigDecimal("123456789.12341234"))
-        .longitude(new BigDecimal("987654321.43214321"))
-        .phone("010xxxxxxxx")
-        .build();
+            .instituteId(1L)
+            .name("도서관")
+            .location("서울시")
+            .latitude(new BigDecimal("37.56100278"))
+            .longitude(new BigDecimal("126.9996417"))
+            .phone("010xxxxxxxx")
+            .build();
 
     @Test
     @DisplayName("새 봉사기관 저장")
@@ -34,6 +35,6 @@ class InstituteJpaRepositoryTest {
         instituteJpaRepository.save(jpaEntity);
         // then
         assertThat(instituteJpaRepository.findById(1L).get().getName())
-            .isEqualTo(jpaEntity.getName());
+                .isEqualTo(jpaEntity.getName());
     }
 }
